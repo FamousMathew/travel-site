3dde3f945cdb16c2bdedea169081bec8f7bf713d
Famous <famous.mathew@gmail.com>
2020-05-04 23:38:14 +0530

    Renamed dist folder to docs

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ class runAfterCompile {
     apply(compiler){
 
         compiler.hooks.done.tap('copy images', function(){
-            fse.copySync('./app/assets/images', './dist/assets/images')
+            fse.copySync('./app/assets/images', './docs/assets/images')
         })
     }
 }
@@ -79,7 +79,7 @@ if (currentTask=='build'){
     config.output = {
         filename: '[name].[chunkhash].js',
         chunkFilename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'docs')
     }
 
     config.mode = 'production'
